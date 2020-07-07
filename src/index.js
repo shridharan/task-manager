c5f149f0b746971bdb62b85c5bcfcb5e02ea6722
shridharan muthu <shri@shridharansmbp2.lan>
2020-07-06 17:04:15 -0700

    moved all routing logic to use express router

--- src/index.js
+++ src/index.js
@@ -1,61 +1,13 @@
 const express = require('express')
-const {Task,findTaskAndUpdateCompleted} = require('./models/task')
 const mongoose = require('./db/mangoose')
+const taskRouter = require('./router/task')
 
 const app = express()
-const port = process.env.PORT || 3000
-
-//consvert body to json from application/json
+//convert body to json from application/json
 app.use(express.json())
+app.use(taskRouter)
 
-app.get('/tasks', async (req, res) => {
-    try {
-        res.send(await Task.find({}))
-    }catch(e) {
-        res.status(500).send(e)
-    }
-})
-
-app.get('/tasks/:name', async (req, res) => {
-    try{
-        res.send(await Task.find({name:req.params.name}))
-    }catch(e) {
-        res.status(500).send(e)
-    }
-})
-
-app.post('/tasks', async (req, res)=> {
-    try{
-        res.status(201).send(await new Task(req.body).save())
-    }catch(e){
-        res.status(400).send(e)
-    }
-})
-
-app.delete('/tasks/:name', async (req, res) => {
-    try {
-        res.send(await findTaskAndUpdateCompleted(req.params.name))
-    }catch(e) {
-        res.status(404).send(e)
-    }
-})
-
-app.patch('/tasks/:id', async (req, res) => {
-    const updates = Object.keys(req.body)
-    const allowedUpdates = ['name','completed']
-    
-    const isAllowedUpdate = updates.every((update)=> allowedUpdates.includes(update))
-    if(!isAllowedUpdate) {
-        return res.status(400).send({error:'Not a valid update!'})
-    }
-    try {
-        res.send(await Task.findByIdAndUpdate(req.params.id,req.body, {new:true}))
-    }catch(e) {
-        res.status(500).send(e)
-    }
-
-})
-
+const port = process.env.PORT || 3000
 app.listen(port, ()=> {
     console.log('Task service listening on',port)
 })
